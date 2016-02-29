bf56021f272efa5dd9b3f75c6df0a50de1465c7b
Eninfosfranco <Eninfosfranco@Eninfosfranco.mare.cl>
2016-02-29 10:13:31 -0300

    primera

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="shortcut Icon" href="./images/icono.ico"/>
+        
+        <title>Escuela Naval</title>
+        
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" />
+        
+        <style>
+            body{
+                background-color: #f9fbfa;
+            } 
+        </style>
+        
+    </head>
+    <body>
+        
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <img src="./images/AnclasArmadaOriginal.png" />
+                </div>
+            </div>
+        </div>
+        
+        
+        <script type="text/javascript" src="./js/bootstrap.js"></script>
+        <script type="text/javascript" src="./js/jquery.js"></script>
+    </body>
+</html>
